e9389db98716d960408fea6c27740d7a1e49b744
Abarna Selvanathan <rnaaba21@gmail.com>
2024-12-22 17:36:04 +0530

    Added crud file

---
+++ CRUD Application/Backend/routes/orderRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { createOrder, getOrders, getOrder, updateOrder, deleteOrder } = require('../controllers/orderController');
+
+const router = express.Router();
+
+router.post('/create', createOrder);
+router.get('/', getOrders);
+router.get('/:id', getOrder);
+router.put('/:id', updateOrder);
+router.delete('/:id', deleteOrder);
+
+module.exports = router;
